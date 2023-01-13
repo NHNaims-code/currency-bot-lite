4437345c194e7244ade8feaeb8baaea150315f1c
NHNaims-code <mdnimurhasan99@gmail..com>
2023-01-13 19:16:30 +0600

    bit change

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 const port = 9000;
+
+let count = 0;
+setInterval(() => {
+  count = count + 1;
+}, 1000)
+
 app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+
+  res.json({ message: "Hello From Express App", count: count });
 });
 
 app.listen(9000, () => {
